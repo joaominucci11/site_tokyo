30ae84d08c9154654684074d6067893ab70a404b
João Pedro Minucci Regueira <159261517+joaominucci11@users.noreply.github.com>
2024-07-31 10:29:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let lamp=  document.getElementById("imagem")
+let legendas= document.getElementById("legenda")
+function imagem1(){
+
+    lamp.src = 'tokyo.jpg'
+    legendas.innerHTML= 'Tokyo'
+    
+}
+function imagem2(){
+
+    lamp. src = 'Shibuya_crossing.png'
+    legendas.innerHTML= 'Shibuya Crossing'
+
+}
+
+function imagem3(){
+
+    lamp.src = 'Templo_sensoji.png'
+    legendas.innerHTML= 'Templo Sensoji'
+}
+function imagem4(){
+
+    lamp. src = 'Tokyo_skytree.png'
+    legendas.innerHTML= 'Tokyo Skytree'
+}
+function imagem5(){
+
+    lamp. src = 'Takeshita_street.png'
+    legendas.innerHTML= 'Takeshita Street'
+}
